5d5142f6357365d35d5f53560edb99a982f8da36
Twaran Gupta <twarangupta01@gmail.com>
2026-07-13 14:35:20 +0530

    Remove legacy resume data hook

--- src/data/useResumeData.js
+++
@@ -1,5 +0,0 @@
-import { resumeSummary, resumeSections, resumeVersions } from './resume';
-
-export function useResumeData() {
-  return { summary: resumeSummary, sections: resumeSections, versions: resumeVersions };
-}
